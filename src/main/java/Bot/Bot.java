86c92cebcf1a4774ad01f02431bff8276bea2b21
Dmytro <kk.doma@gmail.com>
2018-02-27 15:24:02 +0200

    Initial commitqwee

---
+++ src/main/java/Bot/Bot.java
@@ -0,0 +1,53 @@
+package Bot;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class Bot extends TelegramLongPollingBot {
+    public static void main(String[] args) {
+        ApiContextInitializer.init(); // Инициализируем апи
+        TelegramBotsApi botapi = new TelegramBotsApi();
+        try {
+            botapi.registerBot(new Bot());
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+    @Override
+    public String getBotUsername() {
+        return "PopaDimi";
+        //возвращаем юзера
+    }
+
+    @Override
+    public void onUpdateReceived(Update e) {
+        Message msg = e.getMessage(); // Это нам понадобится
+        String txt = msg.getText();
+        if (txt.equals("/start")) {
+            sendMsg(msg, "Hello, world! This is simple bot!");
+        }
+    }
+
+    @Override
+    public String getBotToken() {
+        return "534826550:AAFCaHQFQrq3wzct9QrCC-wY-A9joOsoLWI";
+        //Токен бота
+    }
+    private void sendMsg(Message msg, String text) {
+        SendMessage s = new SendMessage();
+        s.setChatId(msg.getChatId()); // Боту может писать не один человек, и поэтому чтобы отправить сообщение, грубо говоря нужно узнать куда его отправлять
+        s.setText(text);
+        try { //Чтобы не крашнулась программа при вылете Exception
+            sendMessage(s);
+        } catch (TelegramApiException e){
+            e.printStackTrace();
+        }
+    }
+
+}
+
